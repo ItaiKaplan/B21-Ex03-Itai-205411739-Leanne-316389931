6812a55697741ef83dfa40e174e5ad87e839a097
leannejk <leannejk1@gmail.com>
2021-05-26 10:02:33 +0300

    leanne

---
+++ Ex03.ConsoleUI/MainManu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.ConsoleUI
+{
+    static class MainManu
+    {
+        enum eUserInput
+        {
+            AddVehicleToGarage = 1,
+            ShowAllVehicles = 2,
+            ShowVehiclesFilter = 3,
+            ChangeVehicleStatus = 4,
+            FillAirWheels = 5,
+            FillFuelVehicle = 6,
+            FillElectricVehicle = 7,
+            VehicleInfo = 8
+        }
+
+
+
+
+    }
+}
